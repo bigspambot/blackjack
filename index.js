d4e71739641ed8c076e2b87dc05251cc6d217929
bigspambot <chazbish@hotmail.com>
2023-03-08 13:51:31 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let firstCard = 0
+let secondCard = 22
+
+let sum = firstCard + secondCard
+
+// console.log(firstCard)
+// console.log(secondCard)
+// console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to twist? 🙂")
+} else if (sum === 21) {
+    console.log("Yay! You've got Blackjack! 🥳")
+} else {
+    console.log("You're bust! 😭")
+}
+
